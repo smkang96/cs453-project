c7b89c8e55beda5909a6fffdddc7cbd658e2d763
Sungmin Kang <stuatlittle@kaist.ac.kr>
2019-06-05 02:03:49 +0900

    moved things to sut

---
+++ suts/sut.py
@@ -0,0 +1,15 @@
+class C():
+    def __init__(self):
+        pass
+
+    def f(self, a:int, b):
+        if a == b:
+            return a
+        if a > b:
+            return a
+        else:
+            return b
+
+    def g(self):
+        a = 1
+        return a
