212a3460ebf1ac9796806711ca76706be0c8a976
Sungmin Kang <stuatlittle@kaist.ac.kr>
2019-06-04 10:53:40 +0900

    cleanup CLI

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-import ast
-import astor
-import coverage
-from os.path import abspath
-from random import randint
-import sys
+import argparse
 
 from individual import *
 from util import *
 from ga import *
 
-test_candidate = sys.argv[1]
 
+'''
 if test_candidate == 'isbn-validate' :
     fname = abspath('venv/lib/python3.7/site-packages/stdnum/isbn.py')
     modname = 'stdnum.isbn' # for external library module
@@ -29,15 +24,25 @@ if test_candidate == 'triangle' :
     cname = 'Triangle'
     mutname = 'testTriangle'
 
-analyzer = Analyzer(fname, cname)
-ge = GeneticEnvironment(fname, cname, mutname, modname)
-ge.evolve()
+'''
 
-'''
-ind = Individual(
-    fname, cname,
-    [],
-    [],
-    mutname,
-    [ArgInput('int', randint(-10, 10)) for _ in range(2)])
-'''
+def main(parser):
+    fname = abspath(parser.class_file)
+    modname = parser.mod_name
+    cname = parser.class_name
+    mutname = parser.mut_name
+
+    ge = GeneticEnvironment(fname, cname, mutname, modname)
+    ge.evolve()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate Test Data.')
+    parser.add_argument('--class_file', help='file containing class under test',
+                        type=str)
+    parser.add_argument('--class_name', help='name of class under test',
+                        type=str)
+    parser.add_argument('--mod_name', type=str)
+    parser.add_argument('--mut_name', help='name of method under test',
+                        type=str)
+    parser = parser.parse_args()
+    main(parser)
